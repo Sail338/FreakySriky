1e1cc7da8976663162c14d96917357689ba18cdb
ZacharyBys <zachary.bys@gmail.com>
2018-04-07 17:47:24 -0400

    python serial to arduino working

--- HauntedHouse/HauntedHouse.ino
+++ HauntedHouse/HauntedHouse.ino
@@ -1,9 +1,19 @@
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Ready");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  char inMessage = ' ';
 
+  if (Serial.available())
+  {
+    inMessage = Serial.read();
+    Serial.println(inMessage);
+    digitalWrite(12,HIGH);
+  }
+
+  delay(100);
+  
 }
--- arduino.py
+++ arduino.py
@@ -1,2 +1,12 @@
+from time import sleep
 import serial
 
+arduinoOut = serial.Serial('COM6', 9600)
+
+counter = 32
+while True:
+     counter +=1
+     arduinoOut.write(str(chr(counter)).encode()) # Convert the decimal number to ASCII then send it to the Arduino
+     sleep(.1) # Delay for one tenth of a second
+     if counter == 255:
+        counter = 32
