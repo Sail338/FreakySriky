28ed631d76f81fe0d568fc3b7bd6245ec1bdd180
Sail338 <srihari.shankar1@gmail.com>
2018-04-07 17:59:28 -0400

    parsing boyos

--- twitch.py
+++ twitch.py
@@ -9,10 +9,25 @@ def grabOauth():
 
 
 def connectToTwitch():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(("irc.chat.twitch.tv",6667))
-    client.send("< PASS" + config.OAUTH_TOKEN)
-    client.send("< NICK Sail338")
+    try:
+        client = socket.socket()
+        dat = ("irc.chat.twitch.tv",6667)
+        client.connect( ("irc.chat.twitch.tv",6667))
+        message_user = 'NICK sail338\r\n'
+        message = "PASS " + config.OAUTH_TOKEN +"\r\n"
+        client.sendto(message.encode('utf-8'),dat)
+        client.sendto(message_user.encode('utf-8'),dat)
+        client.sendto('JOIN #sail338 \r\n'.encode(),dat)
+        while True:
+            data = client.recv(1024)
+            if data != "b''":
+                #Parse data
+                data = str(data)
+                parsed = data.split(":")
+                if len(parsed) >2:
+                    print(parsed[2])
+    except Exception as e:
+        print(str(e))
 
 connectToTwitch()
 
