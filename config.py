ed0fe319f01f06c3129b43f1b95b1571966234a6
Zachary Bys <zachary.bys@gmail.com>
2018-04-08 10:46:42 -0400

    Hiding config

--- config.py
+++ config.py
@@ -1,7 +1,1 @@
-CLIENT_ID = '5sos92339i8i9avvpany7eet5quu1z'
-CLIENT_SECRET = 'rxy8bfckttdl7rq1wonjq5p1i8ax8n'
-OAUTH_TOKEN = "oauth:h9tibtah0omtbv148vc6o2f12jxdrb"
-WORKSPACE_ID = "88074225-e57c-4643-8fd1-5655a280d11a"
-CREDENTIALS_NAME = "conversation-lztro-conversati-1517062688566"
-USERNAME = "2fda1552-2709-4149-b535-cdfe3a59944f"
-PASSWORD = "lgqT1suoEr58"
+
