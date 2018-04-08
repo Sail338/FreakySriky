fff5b04e18298b16fd8d689bf82683bbb95a0d08
Sail338 <srihari.shankar1@gmail.com>
2018-04-08 02:39:00 -0400

    polling?

--- twitch.py
+++ twitch.py
@@ -4,14 +4,40 @@ import socket
 import re
 import arduino
 import watson_developer_cloud
+import threading
+import operator
 
 intents = ['Ghost','Corpse','LightsOut','Flicker','Noise']
 def grabOauth():
     a = requests.get("https://id.twitch.tv/oauth2/authorize?client_id=" + config.CLIENT_ID +"&redirect_uri=http://localhost&response_type=token%20id_token &scope=openid + chat_login+channel_editor")
     print(a)
+global count_ds
+count_ds = {}
+
+def clearDS():
+    for i in intents:
+        count_ds[i] = 0
+
+clearDS()
+
+def printit():
+  global count_ds
+  ds = count_ds
+  threading.Timer(3.0, printit).start()
+  if len(ds) >0:
+     check = max(ds.items(), key=operator.itemgetter(1))[0]
+     print("BEFORE" +str(count_ds))
+     if(count_ds[check] != 0):
+#        arduino.play_out_intent(check)
+        clearDS()
+        print("AFRER" + str(count_ds))
+  else:
+     print("nobody has polled yet")
+  
 
 
 def connectToTwitch():
+    global count_ds
     try:
         client = socket.socket()
         dat = ("irc.chat.twitch.tv",6667)
@@ -28,8 +54,12 @@ def connectToTwitch():
         )
 
         workspace_id = config.WORKSPACE_ID
-
+        #start polling
+        printit()
         while True:
+            #poll for 5 seconds
+            did_pool = False
+
             data = client.recv(1024)
             print(data)
             if data != "b''":
@@ -48,10 +78,12 @@ def connectToTwitch():
                         }
                     )
                     intent = response['output']['text']
+
                     print(intent)
                     if check in intents:
-                        print("GUCCI")
-                        arduino.play_out_intent(check)
+                        print("Found something to increment")
+                        count_ds[check] +=1                        
+                         
 
     except Exception as e:
         print(str(e))
